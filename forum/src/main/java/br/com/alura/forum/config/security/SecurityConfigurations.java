d9b15ea870bc0096bb4b1eb908bc86732917ba74
TiagoViniciusF <tiago_vinicius_ferreira@hotmail.com>
2022-07-23 18:59:16 -0300

    Implementando a logica de autenticaçao de usuario

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,7 @@
-package br.com.alura.forum.config;
+package br.com.alura.forum.config.security;
 
+import br.com.alura.forum.config.security.AutenticacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,10 +9,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaoService AutenticacaoService;
 
     //configurações de autorização
     @Override
@@ -25,12 +31,11 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     //COnfigurações de autenticação
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.userDetailsService(AutenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     //Configuração de recursos estaticos(js, css, imagens, etc.)
     @Override
     public void configure(WebSecurity web) throws Exception {
-
     }
 }
